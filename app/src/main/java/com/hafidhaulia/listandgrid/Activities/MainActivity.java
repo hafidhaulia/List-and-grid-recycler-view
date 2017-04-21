c13fdcb2e617ac60b03ac20cca619fa37ce7683f
hafidhaulia <hafidhwirandi@gmail.com>
2017-04-21 09:23:54 +0700

    add resources

--- app/src/main/java/com/hafidhaulia/listandgrid/Activities/MainActivity.java
+++ app/src/main/java/com/hafidhaulia/listandgrid/Activities/MainActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
@@ -99,31 +98,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     void getData() {
-        Pokemon pokemon;
-        pokemon = new Pokemon("Pikachu",20,R.drawable.pikachu);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Abra",22,R.drawable.abra);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Bulbasaur",18,R.drawable.bullbasaur);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Eevee",15,R.drawable.eevee);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Jigglypuff",12,R.drawable.jigglypuff);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Mankey",26,R.drawable.mankey);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Meowth",13,R.drawable.meowth);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Pidgey",16,R.drawable.pidgey);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Psyduck",6,R.drawable.psyduck);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Snorlax",30,R.drawable.snorlax);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Venonat",15,R.drawable.venonat);
-        pokemonArrayList.add(pokemon);
-        pokemon = new Pokemon("Zubat",14,R.drawable.zubat);
-        pokemonArrayList.add(pokemon);
+        pokemonArrayList.add(new Pokemon("Pikachu",20,R.drawable.pikachu));
+        pokemonArrayList.add(new Pokemon("Abra",22,R.drawable.abra));
+        pokemonArrayList.add(new Pokemon("Bulbasaur",18,R.drawable.bullbasaur));
+        pokemonArrayList.add(new Pokemon("Eevee",15,R.drawable.eevee));
+        pokemonArrayList.add(new Pokemon("Jigglypuff",12,R.drawable.jigglypuff));
+        pokemonArrayList.add(new Pokemon("Mankey",26,R.drawable.mankey));
+        pokemonArrayList.add(new Pokemon("Meowth",13,R.drawable.meowth));
+        pokemonArrayList.add(new Pokemon("Pidgey",16,R.drawable.pidgey));
+        pokemonArrayList.add(new Pokemon("Psyduck",6,R.drawable.psyduck));
+        pokemonArrayList.add(new Pokemon("Snorlax",30,R.drawable.snorlax));
+        pokemonArrayList.add(new Pokemon("Venonat",15,R.drawable.venonat));
+        pokemonArrayList.add(new Pokemon("Zubat",14,R.drawable.zubat));
+        pokemonArrayList.add(new Pokemon("Weedle",17,R.drawable.weedle));
+        pokemonArrayList.add(new Pokemon("Caterpie",14,R.drawable.caterpie));
+        pokemonArrayList.add(new Pokemon("Rattata",15,R.drawable.rattata));
+        pokemonArrayList.add(new Pokemon("Mew",8,R.drawable.mew));
+        pokemonArrayList.add(new Pokemon("Bellsprout",16,R.drawable.bellsprout));
+        pokemonArrayList.add(new Pokemon("Dratini",19,R.drawable.dratini));
+        pokemonArrayList.add(new Pokemon("Squirtle",20,R.drawable.squirtle));
+        pokemonArrayList.add(new Pokemon("Charmander",17,R.drawable.charmander));
         setCardData();
     }
 
